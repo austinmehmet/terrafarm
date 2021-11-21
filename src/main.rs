7e35cce97951dae52857ac295edfdabd7efd21e9
Austin Mehmet <mehmetaustin@gmail.com>
2021-11-21 01:21:20 -0600

    add writing files to disk

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
+use bytes::Bytes;
+use log::{info};
+use serde_json::{json};
 use std::collections::HashMap;
 use std::error::Error;
 use std::fs::File;
+use std::fs;
 use std::io::BufReader;
-
-use log::{info};
-
-use serde_json::{json};
+use std::io::prelude::*;
 
 fn main() {
     let cloud_provider = std::env::args().nth(1).expect("no cloud provider provided");
@@ -25,14 +26,22 @@ fn main() {
 
     let services_json = json!(services_raw);
     let service_type_uri = &services_json[&service_type].as_str().unwrap();
-    println!("URI Value - {:?}", service_type_uri);
 
     println!("Retrieving template for {}...", service_type);
     debug(format!("Request being directed to {}", service_type_uri));
 
-    make_request(&service_type_uri);
-    // TODO - save file into users /terraform/{service_type} directory
-    std::process::exit(0);
+    let file_bytes = match make_request(&service_type_uri) {
+        Ok(inner) => inner,
+        Err(_) => panic!("Failure on network request")
+    };
+
+    println!("Writing template file to disk...");
+    debug(format!("File bytes length: {}", file_bytes.len()));
+
+    match write_file(file_bytes, &service_type) {
+        Ok(_) => std::process::exit(0),
+        Err(_) => std::process::exit(1),
+    };
 }
 
 fn read_dictionary() -> Result<HashMap<String, serde_json::Value>, Box<dyn Error>> {
@@ -43,10 +52,24 @@ fn read_dictionary() -> Result<HashMap<String, serde_json::Value>, Box<dyn Error
     Ok(map)
 }
 
-fn make_request(uri: &str) {
-    let resp = reqwest::blocking::get(uri);
-    println!("{:?}", resp);
 
+fn make_request(uri: &str) -> Result<bytes::Bytes, reqwest::Error> {
+    let client = reqwest::blocking::Client::new();
+    let response = client.get(uri).send()?;
+    return response.bytes()
+}
+
+fn write_file(bytes: Bytes, service_type: &str) -> std::io::Result<()> {
+    fs::create_dir_all("./terraform")?;
+    let file_name = format!("./terraform/{}.tf", service_type);
+    let mut pos = 0;
+    let mut buffer = File::create(file_name)?;
+
+    while pos < bytes.len() {
+        let bytes_written = buffer.write(&bytes[pos..])?;
+        pos += bytes_written;
+    }
+    Ok(())
 }
 
 fn debug(msg: String) {
